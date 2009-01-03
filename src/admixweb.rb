677ad08a143fc5565c3fcbe5c3a2002865f7b6cd
Josh Lee <jleedev@gmail.com>
2009-01-03 03:58:41 -0500

    FUCK YEAH it exposes rudimentary functionality.

--- src/admixweb.rb
+++ src/admixweb.rb
@@ -1,7 +1,20 @@
+$: << File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib')
+
 require 'rubygems'
 require 'sinatra'
 
-get '/' do <<EOHTML
+require 'admix'
+
+module Admix
+
+  get '/' do <<EOHTML
 Let's mix up some ads.
 EOHTML
+  end
+
+  post '/admix' do |*args|
+    admix = AdmixWrapper.new :loc => params[:loc], :ped => params[:ped]
+    admix.out
+  end
+
 end
