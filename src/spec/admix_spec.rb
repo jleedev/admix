b0561e4e336de4d4e3f7f37583c0f2ba97e10ddd
Josh Lee <jleedev@gmail.com>
2009-01-03 02:50:49 -0500

    Successfully reporting admix's errors.

--- src/spec/admix_spec.rb
+++ src/spec/admix_spec.rb
@@ -5,7 +5,11 @@ describe Admix::AdmixWrapper do
     loc,ped,out = %w(loc ped out).collect do |ext|
       File.read "orig/Test/admix-test." + ext
     end
-    @admix = Admix::AdmixWrapper.new :loc => loc, :ped => ped
-    @admix.out.should == out
+    admix = Admix::AdmixWrapper.new :loc => loc, :ped => ped
+    admix.out.should == out
+  end
+
+  it "should complain on empty input" do
+    lambda { Admix::AdmixWrapper.new :loc => "", :pred => "" }.should raise_error Admix::AdmixError
   end
 end
