d134de2300060b6b2d54bf754ab03da2fa6ace2a
Steven Dee <steve@smartercode.net>
2009-01-03 03:26:06 -0500

    remove Admix::

--- src/spec/admix_spec.rb
+++ src/spec/admix_spec.rb
@@ -1,17 +1,21 @@
 require 'admix'
 
-describe Admix::AdmixWrapper do
-  it "should pass the admix tests" do
-    loc,ped,out = %w(loc ped out).collect do |ext|
-      File.read "orig/Test/admix-test." + ext
+module Admix
+
+  describe AdmixWrapper do
+    it "should pass the admix tests" do
+      loc,ped,out = %w(loc ped out).collect do |ext|
+        File.read "orig/Test/admix-test." + ext
+      end
+      admix = AdmixWrapper.new :loc => loc, :ped => ped
+      admix.out.should == out
     end
-    admix = Admix::AdmixWrapper.new :loc => loc, :ped => ped
-    admix.out.should == out
+
+    it "should complain on empty input" do
+      lambda {
+        AdmixWrapper.new :loc => "", :pred => ""
+      }.should raise_error AdmixError
+    end
   end
 
-  it "should complain on empty input" do
-    lambda {
-      Admix::AdmixWrapper.new :loc => "", :pred => ""
-    }.should raise_error Admix::AdmixError
-  end
 end
