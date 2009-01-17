0494fd7cc035ac5aa6c5a90303e0258a820fff1e
Steven Dee <steve@smartercode.net>
2009-01-16 22:10:56 -0500

    remove admixweb

--- src/lib/admixweb/prettify.rb
+++
@@ -1,9 +0,0 @@
-module AdmixWeb
-
-  def self.prettify str
-    str.split("\n").map do |line|
-      "<tr><td>" + line.gsub(/\ +/,"<td>")
-    end.join("\n")
-  end
-
-end
--- src/spec/admix_spec.rb
+++ src/spec/admix_spec.rb
@@ -29,39 +29,3 @@ describe Admix::Wrapper do
     }.should raise_error Admix::AdmixError
   end
 end
-
-describe AdmixWeb do
-  it "should have a / page" do
-    get_it "/"
-    @response.should be_ok
-  end
-
-  it "should also pass the admix test" do
-    run_test do |data|
-      data.update :type => :text
-      post_it "/admix", data
-      @response.should be_ok
-      @response.content_type.should == 'text/plain'
-      @response.body
-    end
-  end
-
-  it "should return some html" do
-    run_test do |data|
-      data.update :type => :html
-      post_it "/admix", data
-      @response.should be_ok
-      @response.content_type.should == 'text/html'
-      throw :no_results
-    end
-  end
-
-  it "should report errors" do
-    post_it "/admix"
-    @response.should_not be_ok
-  end
-
-  it "should prettify" do
-    (AdmixWeb::prettify "A   B  C\n1  2 3\n").should == "<tr><td>A<td>B<td>C\n<tr><td>1<td>2<td>3"
-  end
-end
