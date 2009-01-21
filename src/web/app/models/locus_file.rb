ed970387799c10fa45f52f35386c9357cef30c6e
Josh Lee <jleedev@gmail.com>
2009-01-20 22:49:48 -0500

    Added has_many relationships.
    Because Rails is too dumb to figure those out.

--- src/web/app/models/locus_file.rb
+++ src/web/app/models/locus_file.rb
@@ -1,3 +1,4 @@
 class LocusFile < ActiveRecord::Base
   belongs_to :user
+  has_many :marker
 end
--- src/web/app/models/marker.rb
+++ src/web/app/models/marker.rb
@@ -1,3 +1,4 @@
 class Marker < ActiveRecord::Base
   belongs_to :locus_file
+  has_many :allele_frequency
 end
