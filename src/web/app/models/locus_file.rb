b057a47cf0f6f2fec82e342426cf76539db1018f
Josh Lee <jleedev@gmail.com>
2009-01-28 18:56:31 -0500

    Added minimum population

--- src/web/app/models/locus_file.rb
+++ src/web/app/models/locus_file.rb
@@ -2,5 +2,5 @@ class LocusFile < ActiveRecord::Base
   belongs_to :user
   has_many :marker
   validates_presence_of :name,:population
-  validates_numericality_of :population
+  validates_numericality_of :population, :greater_than_or_equal_to => 2
 end
