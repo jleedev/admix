779680ada42e7d54ef91dbcb1c51fa019179ae8b
Josh Lee <jleedev@gmail.com>
2009-01-28 17:01:51 -0500

    Added accounts controller and an app layout

--- src/web/app/controllers/accounts_controller.rb
+++ src/web/app/controllers/accounts_controller.rb
@@ -1,2 +1,14 @@
 class AccountsController < ApplicationController
+  def login
+    @title = "Log in"
+    if request.post?
+      @current_user = User.find_by_name params[:login]
+      if @current_user
+        session[:user_id] = @current_user.id
+        redirect_to :controller => :marker_file
+      else
+        flash[:error] = "Incorrect username"
+      end
+    end
+  end
 end
--- src/web/app/controllers/marker_file_controller.rb
+++ src/web/app/controllers/marker_file_controller.rb
@@ -1,5 +1,6 @@
 class MarkerFileController < ApplicationController
   def index
+    @title = "Marker files"
     @files = LocusFile.find :all, :order => 'updated_at DESC'
   end
 end
