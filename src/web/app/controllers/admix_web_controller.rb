46157f9a6ba161a3cbc3061bb7eb8629b2f15b19
Josh Lee <jleedev@gmail.com>
2009-02-15 20:51:12 -0500

    basic AdmixWeb controller for GenotypeFiles.

--- src/web/app/controllers/admix_web_controller.rb
+++ src/web/app/controllers/admix_web_controller.rb
@@ -1,2 +1,9 @@
 class AdmixWebController < ApplicationController
+  def index
+    @title = "Admix web"
+    @genotype_files = GenotypeFile.find :all, :order => 'updated_at DESC'
+  end
+
+  def create
+  end
 end
--- src/web/app/models/genotype_file.rb
+++ src/web/app/models/genotype_file.rb
@@ -1,2 +1,4 @@
 class GenotypeFile < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :name,:data
 end
