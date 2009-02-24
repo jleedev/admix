05341593fbd8993c5b9a30d1fc04b9d6c76eb4f2
Josh Lee <jleedev@gmail.com>
2009-02-24 01:51:47 -0500

    GENOTYPE FILES. WOOOOOOOO

--- src/web/app/controllers/admix_web_controller.rb
+++ src/web/app/controllers/admix_web_controller.rb
@@ -5,5 +5,24 @@ class AdmixWebController < ApplicationController
   end
 
   def create
+    @title = "Upload genotype file"
+    @genotype_file = GenotypeFile.new params[:genotype_file]
+    @genotype_file.data = @genotype_file.data.read if @genotype_file.data
+    if request.post? and @genotype_file.save
+      redirect_to :action => :index
+    end
+  end
+
+  def show
+    @genotype_file = GenotypeFile.find_by_id params[:id]
+    @title = "Genotype file \"#{@genotype_file.name}\""
+  end
+
+  def create_job
+    @title = "Create job"
+    @job = Job.new params[:job]
+    if request.post? and @job.save
+      redirect_to :action => :index
+    end
   end
 end
